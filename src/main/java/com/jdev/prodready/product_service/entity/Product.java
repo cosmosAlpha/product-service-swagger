595719ceef7ab88f7c500f6253b9293840a4a34e
unknown <arghya2020s@gmail.com>
2026-08-25 00:28:03 +0100

    initial commit

---
+++ src/main/java/com/jdev/prodready/product_service/entity/Product.java
@@ -0,0 +1,33 @@
+package com.jdev.prodready.product_service.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private Long price;
+
+    public Product() {
+    }
+
+    public Product(String name, Long price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Long getPrice() {
+        return price;
+    }
+}
